bfbe4786f7c74be6d853c6e6f80dff53dfa75b1f
r3laty <lolkers.ilya@gmail.com>
2024-01-22 19:20:08 +0300

    Start of making lvl

--- Assets/Scripts/Character/CharacterController.cs
+++ Assets/Scripts/Character/CharacterController.cs
@@ -5,6 +5,8 @@ public class CharacterController : MonoBehaviour
 {
     [HideInInspector] public float DirX;
     
+    [SerializeField] private float scale = 4;
+
     [SerializeField] private float speed = 5;
 
     private Rigidbody2D _rb;
@@ -20,19 +22,19 @@ public class CharacterController : MonoBehaviour
 
     private void FixedUpdate()
     {
-        Vector2 moving = new Vector2(DirX, transform.position.y) * speed;
-        _rb.velocity = moving * Time.fixedDeltaTime;
+        Vector2 moving = new Vector2(DirX * speed * Time.fixedDeltaTime, _rb.velocity.y);
+        _rb.velocity = moving;
         Flip();
     }
     private void Flip()
     {
         if (DirX > 0)
         {
-            transform.localScale = new Vector3(5, 5, 5);
+            transform.localScale = new Vector3(scale, scale, 0);
         }
         else if (DirX < 0)
         {
-            transform.localScale = new Vector3(-5f, 5f, 5f);
+            transform.localScale = new Vector3(-scale, scale, 0);
         }
     }
 }
