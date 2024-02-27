ed958b3fa7b90d813c472787be4f108e0b5ef7af
r3laty <lolkers.ilya@gmail.com>
2024-02-27 19:25:55 +0300

    Makung bosse phase 2
    Make exploison (boss spell)

--- Assets/Scripts/EvilGuys/DarkFairy/Phase2.cs
+++ Assets/Scripts/EvilGuys/DarkFairy/Phase2.cs
@@ -3,6 +3,9 @@ using UnityEngine;
 
 public class Phase2 : Phase1
 {
+    [SerializeField] private float explosionForce = 5;
+    [SerializeField] private float explosionRadius = 3;
+    [SerializeField] private float explosionDamage = 45;
     private void Start()
     {
         StartCoroutine(StartOfBossFight());
@@ -15,9 +18,36 @@ public class Phase2 : Phase1
 
         spellAnim.SetBool("Cast_phase2", true);
     }
+    public new void BackToIdle()
+    {
+        bossAnim.SetBool("Explode", false);
+    }
+
     private void CastingSpell()
     {
         bossAnim.SetBool("Casting", true);
+    }
+    private void Explode()
+    {
+        bossAnim.SetBool("Explode", true);
+        Collider2D[] overlappedColliders = Physics2D.OverlapCircleAll(transform.position, explosionRadius);
+
+        foreach (Collider2D collider in overlappedColliders)
+        {
+            Rigidbody2D rigidbody = collider.attachedRigidbody;
+            if (rigidbody)
+            {
+                Vector2 direction = rigidbody.position - (Vector2)transform.position;
+                rigidbody.AddForce(direction.normalized * explosionForce, ForceMode2D.Impulse);
+            }
+
+            CharacterHp characterHp = collider.GetComponent<CharacterHp>();
+            if (characterHp != null)
+            {
+                characterHp.TakeDamage(explosionDamage);
+                Debug.Log(characterHp.name + " character hp found");
+            }
+        }
     }
     private IEnumerator StartOfBossFight()
     {
@@ -27,6 +57,8 @@ public class Phase2 : Phase1
         {
             yield return new WaitForSeconds(timeToNextAttack);
             CastingSpell();
+            yield return new WaitForSeconds(timeToNextAttack);
+            Explode();
         }
     }
 
